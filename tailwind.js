e11160fbfa45ae18edf8d1239345bb86363edcb3
guy <devtyl3r@gmail.com>
2022-12-15 21:31:47 -0400

    copied to clipboard

--- tailwind.js
+++ tailwind.js
@@ -1,10 +1,29 @@
 const header = document.querySelector('header');
+const placement = document.querySelector('.header__random-btn');
 
 function addStyles(element, styles) {
    Object.keys(styles).forEach((styleKey) => {
       element.style[styleKey] = styles[styleKey];
    });
 }
+
+const button = document.createElement('button');
+addStyles(button, {
+   marginRight: '0.25rem',
+   cursor: 'pointer',
+   opacity: '95%',
+   backgroundColor: '#06B6D4',
+   padding: '0.5rem 1rem',
+   borderRadius: '1.5rem',
+   border: '1px solid #06B6D4',
+   fontWeight: '600',
+   fontFamily: '"Roboto Mono", monospace',
+   letterSpacing: '-0.025em',
+   boxShadow: '0 25px 50px -12px rgb(0 0 0 / 0.25)',
+});
+button.textContent = 'Tailwind';
+button.onclick = handler;
+header.insertBefore(button, placement);
 
 function handler() {
    const colors = Array.from(
@@ -40,22 +59,19 @@ function handler() {
          )
       )
    );
+   const copied = document.createElement('div');
+   addStyles(copied, {
+      fontSize: '0.75rem',
+      lineHeight: '1rem',
+      fontFamily: '"Roboto Mono", monospace',
+      marginRight: '1rem',
+      letterSpacing: '-0.025em',
+      userSelect: 'none',
+      pointerEvents: 'none',
+   });
+   copied.textContent = 'Copied to clipboard';
+   header.insertBefore(copied, button);
+   setTimeout(() => {
+      copied.remove();
+   }, 2000);
 }
-
-const button = document.createElement('button');
-addStyles(button, {
-   marginRight: '0.25rem',
-   cursor: 'pointer',
-   opacity: '95%',
-   backgroundColor: '#06B6D4',
-   padding: '0.5rem 1rem',
-   borderRadius: '1.5rem',
-   border: '1px solid #06B6D4',
-   fontWeight: '600',
-   fontFamily: '"Roboto Mono", monospace',
-   letterSpacing: '-0.025em',
-   boxShadow: '0 25px 50px -12px rgb(0 0 0 / 0.25)',
-});
-button.textContent = 'Tailwind';
-button.onclick = handler;
-header.insertBefore(button, document.querySelector('.header__random-btn'));
